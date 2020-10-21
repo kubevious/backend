a270851064b553dd6246d4226247f34ec7fea558
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-10-20 17:27:17 -0700

    run script.

---
+++ run-dev-remote-worldvious.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+MY_DIR="$(dirname $MY_PATH)"
+cd $MY_DIR
+
+export LOG_TO_FILE=true
+export NODE_ENV=development
+# export DEBUG=express:*
+#  --max_old_space_size=2048
+export MYSQL_HOST=localhost
+export MYSQL_PORT=4033
+export MYSQL_USER=root
+export MYSQL_PASS=
+export MYSQL_DB=kubevious
+
+export WORLDVIOUS_URL=https://api.worldvious.io/api/v1/oss
+export WORLDVIOUS_ID=123e4567-e89b-12d3-a456-426614174000
+
+export WORLDVIOUS_VERSION_CHECK_TIMEOUT=5
+export WORLDVIOUS_COUNTERS_REPORT_TIMEOUT=6
+export WORLDVIOUS_METRICS_REPORT_TIMEOUT=6
+export WORLDVIOUS_ERROR_REPORT_TIMEOUT=7
+
+node src/
