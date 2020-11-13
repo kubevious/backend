fe7c86ab2f9ae81a264038013522345d55a2d7a6
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-11-12 20:25:01 -0800

    changes to db init.

---
+++ mysql/db-init.sql
@@ -0,0 +1,4 @@
+USE kubeivous;
+
+SET NAMES utf8;
+SET GLOBAL binlog_expire_logs_seconds = 259200;
--- src/lib/db/migrators/1.js
+++ src/lib/db/migrators/1.js
@@ -3,14 +3,6 @@ const Promise = require('the-promise');
 module.exports = function(logger, driver, executeSql) {
 
     var queries = [
-        "SET NAMES utf8;"
-        
-        ,
-
-        "SET GLOBAL binlog_expire_logs_seconds = 259200;"
-        
-        ,
-
         "CREATE TABLE IF NOT EXISTS `config` (" +
             "`key` varchar(64) NOT NULL DEFAULT ''," +
             "`value` json NOT NULL," +
