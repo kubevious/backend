70f31f4b937dc8014809a6051fcac40646e6b52f
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-12-28 11:21:27 -0800

    updated mysql start script.

--- mysql/start.sh
+++ mysql/start.sh
@@ -3,4 +3,6 @@ MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE
 MY_DIR="$(dirname $MY_PATH)"
 cd $MY_DIR
 
+docker network create kubevious
+
 docker-compose up -d --build
