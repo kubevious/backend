75fcded415494e25756d99b6db8306a73e189d7d
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-04-14 09:52:54 -0700

    fixes to docker run.

--- run-docker.sh
+++ run-docker.sh
@@ -9,14 +9,18 @@ docker run \
     -it \
     --rm \
     --name "${CONTAINER_NAME}" \
+    -h ${CONTAINER_NAME} \
     -p ${SERVER_PORT}:${SERVER_PORT} \
     --network ${NETWORK_NAME} \
     -e NODE_ENV=development \
+    -e SERVER_PORT=${SERVER_PORT} \
     -e MYSQL_HOST=kubevious-mysql \
-    -e MYSQL_PORT=3306 \
-    -e MYSQL_DB=kubevious \
-    -e MYSQL_USER=root \
-    -e MYSQL_PASS= \
+    -e MYSQL_DB=${MYSQL_DB} \
+    -e MYSQL_USER=${MYSQL_USER} \
+    -e MYSQL_PASS=${MYSQL_PASS} \
+    -e REDIS_HOST=kubevious-redisearch \
+    -e COLLECTOR_BASE_URL=http://kubevious-collector:4003 \
+    -e PARSER_BASE_URL=http://kubevious-parser:4004 \
     ${IMAGE_NAME}
 
     
